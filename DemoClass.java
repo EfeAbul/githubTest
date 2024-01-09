0f1ef2202178cfab0faf9b2ba4f517997b8ab5e1
Efe BUL <ebul@innova.com.tr>
2024-01-09 12:12:07 +0300

    added DemoClass
    some description

---
+++ DemoClass.java
@@ -0,0 +1,7 @@
+package githubTest;
+
+public class DemoClass {
+    public static void main(String[] args) {
+        System.out.println("demo class for github");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# githubTest
+# Demo
 This repository is created for test purposes!
+
+## Subheader
+
+This is a demo
